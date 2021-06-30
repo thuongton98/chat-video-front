7f9f8c5d2349986018fa42487d4ef8b9765adde4
thuong <thuonglovevietnam@gmail.com>
2021-06-30 13:23:30 +0700

    t

--- src/components/page/testvideo.js
+++ src/components/page/testvideo.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import Peer from 'peerjs'
 import io from 'socket.io-client';
-import {useState,useRef} from 'react'
+import {useState,useRef,useEffect} from 'react'
+import { Redirect } from 'react-router';
 
-const socket= io('https://thuongchat.tk', { transports: ['websocket', 'polling', 'flashsocket'] })
+const socket= io('http://localhost:5000', { transports: ['websocket', 'polling', 'flashsocket'] })
 const myPeer = new Peer({host:'thuongchat.tk', secure:true, port:443, path: '/peerjs/myapp'})
 
 function Video(){
 
-    
+    //change room id
     let  ROOM_ID='123123'
     var videoz = useRef('')
     var videozz=useRef('')
-
-
    
-    
+  
+useEffect(()=>{
+    socket.on('accept',data=>{
+        videoz.className='video default'
+        videozz.className='video user'
+       
+    })
+   
+    socket.on('onroom', data => {
+       if(data.length>1){
+        videoz.className='video default'
+        videozz.className='video user'
+       }
+    })
+ 
+},[])
+   
+
 
 navigator.mediaDevices.getUserMedia({
     video: false,
@@ -35,13 +51,16 @@ navigator.mediaDevices.getUserMedia({
    
         socket.on('user-connected', userId => {
            
-           
-           
+         
+           socket.emit('accept',ROOM_ID)
+        
             connectToNewUser(userId, stream)
            
         })
         socket.on('user-disconnected', data=>{
-         
+            videoz.className='video user'
+               videozz.className='none'
+              
         })
     
 
@@ -49,8 +68,11 @@ navigator.mediaDevices.getUserMedia({
 
 myPeer.on('open', id => {
 const data = {
+    //change room id
   ROOM_ID,
-  id
+  id,
+  // change name
+  name:'thuong'
 }
     if(socket!==''){
         socket.emit('join-room',data)
@@ -81,11 +103,15 @@ function addVideoStream(video, stream) {
    
    
 }
+
     return(
         <section className="p404">
         <h1>test video</h1>
-        <video className={'video user'} autoPlay playsInline ref={ref=>videoz=ref} ></video>
-        <video className={'video user'} autoPlay playsInline ref={ref=>videozz=ref}></video>
+       
+       
+        <video className='video user' autoPlay playsInline ref={ref=>videoz=ref} ></video>
+        <video className='video none' autoPlay playsInline ref={ref=>videozz=ref}></video>
+       
       </section>
     )
 }
